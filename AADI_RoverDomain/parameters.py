d954df6d8ced03642d2a30afb16bd7e7a0ec3f64
Connor Yates <yatesco@oregonstate.edu>
2019-09-19 20:36:00 -0700

    Adds the ability to load/save parameters from a yaml to the Parameters object. Needs to use all the parameters as object members, not class members so vars() loads all instances properly.

--- AADI_RoverDomain/parameters.py
+++ AADI_RoverDomain/parameters.py
@@ -2,6 +2,7 @@
 This file contains all constant test parameters which may be altered from this single
 location for convenience.
 """
+import yaml
 
 
 class Parameters:
@@ -10,38 +11,76 @@ class Parameters:
     TODO: Heterogeneous rover domain code currently not implemented
     """
 
-    # Run Parameters
-    stat_runs = 10
-    generations = 1000  # Number of generations for CCEA in each stat run
-    new_world_config = False  # False -> Reuse existing world config, True -> Use new world config
+    def __init__(self):
+        # Run Parameters
+        self.stat_runs = 10
+        self.generations = 1000  # Number of generations for CCEA in each stat run
+        self.new_world_config = False  # False -> Reuse existing world config, True -> Use new world config
 
-    # Visualizer
-    running = False  # True keeps visualizer from closing until you 'X' out of window
+        # Visualizer
+        self.running = False  # True keeps visualizer from closing until you 'X' out of window
 
-    # Domain parameters
-    team_types = 'homogeneous'  # Switch between 'homogeneous' and 'heterogeneous' rover domains
-    num_rovers = 12  # Number of rovers on map (GETS MULTIPLIED BY NUMBER OF TYPES)
-    coupling = 3  # Number of rovers required to view a POI for credit
-    num_pois = 10  # Number of POIs on map
-    num_steps = 20  # Number of steps rovers take each episode
-    min_distance = 1.0  # Minimum distance which may appear in the denominator of credit eval functions
-    x_dim = 30  # X-Dimension of the rover map
-    y_dim = 30  # Y-Dimension of the rover map
-    min_observation_dist = 3.0  # Minimum distance rovers must be to observe POIs
-    angle_resolution = 90  # Resolution of sensors (determines number of sectors)
-    sensor_model = "summed"  # Should either be "density" or "closest" or "summed"
+        # Domain parameters
+        self.team_types = 'homogeneous'  # Switch between 'homogeneous' and 'heterogeneous' rover domains
+        self.num_rovers = 12  # Number of rovers on map (GETS MULTIPLIED BY NUMBER OF TYPES)
+        self.coupling = 3  # Number of rovers required to view a POI for credit
+        self.num_pois = 10  # Number of POIs on map
+        self.num_steps = 20  # Number of steps rovers take each episode
+        self.min_distance = 1.0  # Minimum distance which may appear in the denominator of credit eval functions
+        self.x_dim = 30  # X-Dimension of the rover map
+        self.y_dim = 30  # Y-Dimension of the rover map
+        self.min_observation_dist = 3.0  # Minimum distance rovers must be to observe POIs
+        self.angle_resolution = 90  # Resolution of sensors (determines number of sectors)
+        self.sensor_model = "summed"  # Should either be "density" or "closest" or "summed"
 
-    # Neural network parameters
-    num_inputs = 8
-    num_nodes = 9
-    num_outputs = 2
+        # Neural network parameters
+        self.num_inputs = 8
+        self.num_nodes = 9
+        self.num_outputs = 2
 
-    # CCEA parameters
-    mutation_rate = 0.1  # Probability that a member of the offspring population will be mutated
-    percentage_mut = 0.01  # Percentage of bits which get flipped in an individual
-    epsilon = 0.1  # For e-greedy selection in CCEA
-    parent_pop_size = 20
-    offspring_pop_size = 5
+        # CCEA parameters
+        self.mutation_rate = 0.1  # Probability that a member of the offspring population will be mutated
+        self.percentage_mut = 0.01  # Percentage of bits which get flipped in an individual
+        self.epsilon = 0.1  # For e-greedy selection in CCEA
+        self.parent_pop_size = 20
+        self.offspring_pop_size = 5
 
-    # User specific parameters
-    reward_type = "SDPP"  # Switch between reward functions "Global" "Difference" "DPP" "SDPP"
+        # User specific parameters
+        self.reward_type = "SDPP"  # Switch between reward functions "Global" "Difference" "DPP" "SDPP"
+
+    def load_yaml(self, filename):
+        """
+        loads a set of parameters into this object with setattr
+        Does not require that all parameters are set, i.e. you can optionally set a specific set of parameters
+        :param filename:
+        :return:
+        """
+        with open(file=filename) as f:
+            params = yaml.load(f)
+        for key in params:
+            setattr(self, key, params[key])
+
+    def save_yaml(self, filename):
+        """
+        Saves ALL the parameters into a yaml file, not just the optionally set ones
+        :param filename:
+        :return:
+        """
+        with open(file=filename, mode='w') as f:
+            yaml.dump(vars(self), f)
+
+if __name__ == '__main__':
+    with open("/tmp/param_tests.yaml", 'w') as f:
+        f.write("""
+        test_a: 10
+        stat_runs: 100
+        """)
+    parameters = Parameters()
+    print("Before load")
+    print(parameters.stat_runs)
+    parameters.load_yaml("/tmp/param_tests.yaml")
+    print("After load")
+    print(parameters.test_a)
+    print(parameters.stat_runs)
+    parameters.save_yaml("/tmp/param_tests_out.yaml")
+
